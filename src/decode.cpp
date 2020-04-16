14e076951451c5f2a13dc880e665f8dbcdf7dabd
hedin_hiervard <flame.beholder@gmail.com>
2020-04-16 19:23:26 +0200

    wav file loading with AudioFile

--- src/decode.cpp
+++ src/decode.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
+#include "../lib/audiofile/AudioFile.h"
+
+using namespace std;
+
 auto main(int argc, char** argv) -> int {
+	if(argc < 2) {
+		cout << "usage: decode <wav-file>" << endl;
+		return 0;
+	}
+	const std::string fileName = argv[1];
+	cout << "loading " << fileName << endl;
+
+	AudioFile<double> audioFile;
+	audioFile.load(fileName);
+
+	cout << "loaded file" << endl;
+	audioFile.printSummary();
+
 	return 0;
 }
